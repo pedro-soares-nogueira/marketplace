9111cc9d92c92ed7923760b5fd3a22de8e50b96d
Pedro Soares <dev.pedro.nogueira@gmail.com>
2023-03-26 11:21:59 -0300

    init project

--- App.tsx
+++ App.tsx
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  Poppins_700Bold,
+  Poppins_800ExtraBold,
+  Poppins_900Black,
+} from "@expo-google-fonts/poppins"
+import { StyleSheet, Text, View, StatusBar } from "react-native"
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+    Poppins_800ExtraBold,
+    Poppins_900Black,
+  })
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar
+        barStyle={"dark-content"}
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? (
+        <Text style={{ fontFamily: "Poppins_700Bold" }}>
+          Open up App.tsx to start working on your app!
+        </Text>
+      ) : (
+        <View />
+      )}
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-});
+})
